08931e417860a149d79bcffe87ceee4aeef5d6bc
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-18 23:36:28 +0000

    created function to render page after clicking 'start' button to remove unused elements and display question and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,47 @@
 // 4.3 after each answer show if was correct or wrong 
 // 5. after quiz show score, textbox to save initials
 // 6. highscores page shows initials and score in the list
+
+// Declare all DOM attributes
+var btnStartEl = document.querySelector("#btn-start");
+var pageHeaderEl = document.querySelector("#page-title");
+var welcomeContentEl = document.querySelector("#welcome-content")
+var mainContentEl = document.querySelector("#main-content");
+
+// Declare and create list elements used for answers
+var listEl = document.createElement("ul");
+var firstAnswerEl = document.createElement("li");
+var secondAnswerEl = document.createElement("li");
+var thirdAnswerEl = document.createElement("li");
+var fourthAnswerEl = document.createElement("li");
+
+// Store all questions and answers, question[x][1] to be always correct answer
+const questions = [
+    ["question1", "answer1", "answer2", "answer3", "answer4"],
+    ["question2", "answer1", "answer2", "answer3", "answer4"],
+    ["question3", "answer1", "answer2", "answer3", "answer4"],
+    ["question4", "answer1", "answer2", "answer3", "answer4"],
+    ["question5", "answer1", "answer2", "answer3", "answer4"],
+];
+
+function yetToBeNamed() {
+    welcomeContentEl.remove();
+    btnStartEl.remove();
+
+    mainContentEl.appendChild(listEl);
+    listEl.appendChild(firstAnswerEl).textContent = "<button>" + questions[0][1] + "</button>";
+    listEl.appendChild(secondAnswerEl).textContent = questions[0][2];
+    listEl.appendChild(thirdAnswerEl).textContent = questions[0][3];
+    listEl.appendChild(fourthAnswerEl).textContent = questions[0][4];
+
+    firstAnswerEl.setAttribute("id", "answer-1");
+    secondAnswerEl.setAttribute("id", "answer-2");
+    thirdAnswerEl.setAttribute("id", "answer-2");
+    fourthAnswerEl.setAttribute("id", "answer-4");
+
+};
+
+btnStartEl.addEventListener("click", function() {
+    pageHeaderEl.textContent = questions[0][0];
+    yetToBeNamed();
+});
