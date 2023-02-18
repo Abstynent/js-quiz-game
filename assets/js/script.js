79679e7cd3e477f0dd9658fb93f1483761fb99ee
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-18 22:28:19 +0000

    updated .html and .css to have basic styling. pseudo code for the script

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// 1. landing page with nav bar, left side link to high scores, right side timer
+// 2. maybe some picture in the middle, under button to start the quiz
+// 3. questions are displayed and 4 buttons with answers
+// 4. check submitted answer
+// 4.1 if answered correctly add point, next question
+// 4.2 if answered wrong substract 5 sec from timer, go to another question
+// 4.3 after each answer show if was correct or wrong 
+// 5. after quiz show score, textbox to save initials
+// 6. highscores page shows initials and score in the list
