368aa58acd50cd6f1645755288f1f3097cfe1c6b
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-19 10:17:32 +0000

    check if question was answered correctly and display message in new section below

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // 3. questions are displayed and 4 buttons with answers
 // 4. check submitted answer
 // 4.1 if answered correctly add point, next question
-// 4.2 if answered wrong substract 5 sec from timer, go to another question
+// 4.2 if answered wrong substract 10 sec from timer, go to another question
 // 4.3 after each answer show if was correct or wrong 
 // 5. after quiz show score, textbox to save initials
 // 6. highscores page shows initials and score in the list
@@ -20,6 +20,11 @@ var firstAnswerEl = document.createElement("li");
 var secondAnswerEl = document.createElement("li");
 var thirdAnswerEl = document.createElement("li");
 var fourthAnswerEl = document.createElement("li");
+var firstAnswerBtnEl = document.createElement("button");
+var secondAnswerBtnEl = document.createElement("button");
+var thirdAnswerBtnEl = document.createElement("button");
+var fourthAnswerBtnEl = document.createElement("button");
+var answerResult = document.createElement("section"); // section to show result of the answer
 
 // Store all questions and answers, question[x][1] to be always correct answer
 const questions = [
@@ -30,24 +35,36 @@ const questions = [
     ["question5", "answer1", "answer2", "answer3", "answer4"],
 ];
 
-function yetToBeNamed() {
+// Remove landing page and display first question and answer buttons
+function renderPageToDisplayQuizElements() {
     welcomeContentEl.remove();
     btnStartEl.remove();
-
+    // Create list
     mainContentEl.appendChild(listEl);
-    listEl.appendChild(firstAnswerEl).textContent = "<button>" + questions[0][1] + "</button>";
-    listEl.appendChild(secondAnswerEl).textContent = questions[0][2];
-    listEl.appendChild(thirdAnswerEl).textContent = questions[0][3];
-    listEl.appendChild(fourthAnswerEl).textContent = questions[0][4];
 
-    firstAnswerEl.setAttribute("id", "answer-1");
-    secondAnswerEl.setAttribute("id", "answer-2");
-    thirdAnswerEl.setAttribute("id", "answer-2");
-    fourthAnswerEl.setAttribute("id", "answer-4");
+    // Add list elements
+    listEl.appendChild(firstAnswerEl);
+    listEl.appendChild(secondAnswerEl);
+    listEl.appendChild(thirdAnswerEl);
+    listEl.appendChild(fourthAnswerEl);
+
+    // Add button to list elements
+    firstAnswerEl.appendChild(firstAnswerBtnEl).textContent = questions[0][1];
+    secondAnswerEl.appendChild(secondAnswerBtnEl).textContent = questions[0][2];
+    thirdAnswerEl.appendChild(thirdAnswerBtnEl).textContent = questions[0][3];
+    fourthAnswerEl.appendChild(fourthAnswerBtnEl).textContent = questions[0][4];
+
+    mainContentEl.appendChild(answerResult); //.textContent = "Correct!";
+    answerResult.setAttribute("id", "answer-result");
+    // Not sure if I need that part
+    // firstAnswerBtnEl.setAttribute("id", "btn-answer-1");
+    // secondAnswerBtnEl.setAttribute("id", "btn-answer-2");
+    // thirdAnswerBtnEl.setAttribute("id", "btn-answer-2");
+    // fourthAnswerBtnEl.setAttribute("id", "btn-answer-4");
 
 };
 
 btnStartEl.addEventListener("click", function() {
     pageHeaderEl.textContent = questions[0][0];
-    yetToBeNamed();
+    renderPageToDisplayQuizElements();
 });
